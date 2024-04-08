8b2d91c67c8a4f7a715a2e03db2922e37c4d1b4d
RandyWierdaM <99207218+RandyWierdaM@users.noreply.github.com>
2024-04-08 06:03:32 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31466.556711958885!2d-115.06450404132043!3d54.113354355368536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sca!4v1712456252404!5m2!1sen!2sca"
+              width="600"
+              height="600"
+              frameBorder="0"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              aria-hidden="false"
+              tabIndex="0"
+            />
+    </div>
+  );
+}
