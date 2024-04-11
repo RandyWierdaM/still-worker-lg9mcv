45086c3f86722afb8fd8d73533d8059b9df188a3
randywierdam <99207218+RandyWierdaM@users.noreply.github.com>
2024-04-11 12:50:31 +0000

    updated index.html with code from tutorial
    ran npm install from command line
    
    updated index.js with code from tutorial
    
    created index.css file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import "./styles.css";
+import React, { useRef, useEffect, useState } from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31466.556711958885!2d-115.06450404132043!3d54.113354355368536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sca!4v1712456252404!5m2!1sen!2sca"
-              width="600"
-              height="600"
-              frameBorder="0"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              aria-hidden="false"
-              tabIndex="0"
-            />
-    </div>
-  );
-}
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiY2hhbXBpb25tYXBzIiwiYSI6ImNsdXY4Y3JwNjAwc3kya214MHlxZXFrcmIifQ.DwktiPJMD2dqhEBwQxeXRw"
+
+export default function App() {const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+    return (
+      <div>
+        <div ref={mapContainer} className="map-container" />
+      </div>
+    );  
+  });
