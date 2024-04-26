4e55d3079b343f67f0ad5a059a0162b9da0fd4e0
randywierdam <99207218+RandyWierdaM@users.noreply.github.com>
2024-04-26 19:53:50 +0000

    Migrated to mapbox

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h2>This is a second line</h2>
       <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31466.556711958885!2d-115.06450404132043!3d54.113354355368536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sca!4v1712456252404!5m2!1sen!2sca"
-              width="600"
-              height="600"
-              frameBorder="0"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              aria-hidden="false"
-              tabIndex="0"
-            />
+        src="https://api.mapbox.com/styles/v1/rwierdatow/cl335zxdp000015mxlbpg9omx.html?title=false&access_token=pk.eyJ1IjoicndpZXJkYXRvdyIsImEiOiJja2kwbWgzcjQxNDZhMnNrYjBvb2xoYnFqIn0.5garp2DAn8cP6AFUcgVZQw&zoomwheel=false#12.00/53.68/-114.36"
+        title="Mapbox Outdoors"
+        width="100%"
+        height="600"
+        frameBorder="0"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        aria-hidden="false"
+        tabIndex="0"
+      />
     </div>
   );
 }
